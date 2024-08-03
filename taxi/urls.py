adb2987e225d951773f1675c3c5206f7a7c28cf9
Olena Zaritska <zaritskaos@gmail.com>
2024-08-03 11:39:20 +0300

    improve urls paths

--- taxi/urls.py
+++ taxi/urls.py
@@ -17,9 +17,9 @@ urlpatterns = [
         name="manufacturer-list"
     ),
     path("cars/", CarListView.as_view(), name="car-list"),
-    path("cars/<int:pk>", CarDetailView.as_view(), name="car-detail"),
+    path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path("drivers/<int:pk>", DriverDetailView.as_view(), name="driver-detail"),
+    path("drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"),
 ]
 
 app_name = "taxi"
